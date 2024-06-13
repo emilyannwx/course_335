b2b4d2e95e95ac06d3c4eccede1a09846c606cc6
emilyannwx <121910172+emilyannwx@users.noreply.github.com>
2024-06-13 17:20:12 -0400

    overload the + and [] operators for Points2D class

--- assignments/assgnment1/points2d.h
+++ assignments/assgnment1/points2d.h
@@ -76,6 +76,7 @@ class Points2D {
        std::swap(sequence_, rhs.sequence_); 
     }
 
+    //destructor
     ~Points2D(){
         delete[] sequence_;
         sequence_ = nullptr;
@@ -86,11 +87,12 @@ class Points2D {
 
     // One parameter constructor.
     Points2D(const std::array<Object, 2>& item) {
-        // Provide code.
+        sequence_ = new std::array<Object, 2>[1];
+        sequence_[0] = item;
     }
 
     size_t size() const {
-        // Code missing.
+        return size_
     }
 
     // @location: an index to a location in the sequence.
@@ -98,7 +100,10 @@ class Points2D {
     // const version.
     // abort() if out-of-range.
     const std::array<Object, 2>& operator[](size_t location) const {
-        // Code missing.
+        if (location >= size_) {
+            abort(); 
+        }
+        return sequence_[location];
     }
 
     //  @c1: A sequence.
@@ -106,18 +111,29 @@ class Points2D {
     //  @return their sum. If the sequences are not of the same size, append the
     //    result with the remaining part of the larger sequence.
     friend Points2D operator+(const Points2D &c1, const Points2D &c2) {
-        // Code missing.
+        Points2D result;
+        result.size_ = max(c1.size_, c2.size_);
+        for(int i = 0; i < result.size_; i++){
+            result.sequence_[i] = c1.sequence_[i] + c2.sequence_[i];
+        }
+        return result    
     }
 
     // Overloading the << operator.
     friend std::ostream &operator<<(std::ostream &out, const Points2D &some_points) {
-        // Code missing.
+      for(int i = 0; i < some_points.size_; i++){
+        out << "(", some_points.sequence_[i][0] << ',' << some_points.sequence_[i][1] << ")";
+      }
+      return out;
     }
 
     // Overloading the >> operator.
     // Read a chain from an input stream (e.g., standard input).
     friend std::istream &operator>>(std::istream &in, Points2D &some_points) {
-        // Code missing.
+       for(int i = 0; i < some_points.size_; i++){
+        in << "(", some_points.sequence_[i][0] << ',' << some_points.sequence_[i][1] << ")";
+      }
+      return in; 
     }
 
   private:
