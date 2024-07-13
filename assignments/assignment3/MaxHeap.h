bc0bdff729e403427ea75c3b8852f6ad2790aeac
emilyannwx <121910172+emilyannwx@users.noreply.github.com>
2024-07-13 14:43:42 -0400

    defined functions

--- assignments/assignment3/MaxHeap.h
+++ assignments/assignment3/MaxHeap.h
@@ -13,13 +13,47 @@ class MaxHeap{
             return current_size == 0;
         }
         const Comparable & findMax() const{return array[1];}
-        void insert(const Comparable &x);
-        Comparable deleteMax();
+        void insert(const Comparable &x){
+          if (current_size == array.size() - 1){
+            array.resize(array.size() * 2);
+          }
+          int hole = ++current_size;
+          Comparable copy = x;
+          array[0] = move(copy);
+          for(; x > array[hole/2]; hole/=2){
+            array[hole] = move(array[hole/2]);
+          }
+          array[hole]= move(array[0]);
+        }
+        Comparable deleteMax(){
+            if(isEmpty()){
+                throw runtime_error("Heap is empty");
+                Comparable maxItem = move(array[1]);
+                array[1] = move(array[current_size--]);
+                percolateDown(1);
+                return maxItem;
+            }
+        }
 
     private:
         int current_size;
         vector<Comparable> array;
-        void percolateDown(int hole);
+        void percolateDown(int hole){
+            int child;
+            Comparable tmp = move(array[hole]);
+            for(; hole * 2 <= current_size; hole = child){
+                child = hole *2;
+                if(child != current_size && array[child+1] > array[child]){
+                    ++childl
+                }
+                if(array[child] > tmp){
+                    array[hole] = move(array[child]);
+                }else{
+                    break;
+                }
+                array[hole] = move(tmp);
+            }
+        }
 };
 
 #endif //MAXHEAP.H
